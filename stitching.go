c95c1a50ee05bf2a1430623351100c321bc7ead1
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2024-10-28 17:39:06 +0300

    Added Go-wrapper for Stitcher::Status

--- stitching.go
+++ stitching.go
@@ -12,6 +12,17 @@ type Stitcher_Mode struct {
 	p C.StitcherMode
 }
 
+type Stitcher_Status struct {
+	p C.StitcherStatus
+}
+
+const (
+	Stitcher_Status_OK = iota
+	Stitcher_Status_ERR_NEED_MORE_IMGS
+	Stitcher_Status_ERR_HOMOGRAPHY_EST_FAIL
+	Stitcher_Status_ERR_CAMERA_PARAMS_ADJUST_FAIL
+)
+
 func Stitcher_Create(mode Stitcher_Mode) Stitcher {
 	return Stitcher{p: unsafe.Pointer(C.Stitcher_Create(mode.p))}
 }
